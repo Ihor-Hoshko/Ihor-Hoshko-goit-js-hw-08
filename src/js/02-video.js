c0b66d631eb7a341975912a9e624d554fb582454
Ihor-Hoshko <fata77698@gmail.com>
2021-12-18 22:22:11 +0200

    2 t

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,35 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
+const iframe = document.querySelector('#vimeo-player');
+const player = new Vimeo.Player(iframe);
+const storageKey = 'videoplayer - current - time';
 
+// player.on('play', function () {
+//   console.log('played the video!');
+// });
+
+// player.getVideoTitle().then(function (title) {
+//   console.log('title:', title);
+// });
+
+const onPlay = function ({ seconds }) {
+  localStorage.setItem(storageKey, seconds);
+};
+
+const currentTime = localStorage.getItem(storageKey);
+
+player
+  .setCurrentTime(currentTime)
+  .then(function (value) {
+    value = currentTime;
+  })
+  .catch(function (error) {
+    switch (error.name) {
+      case 'RangeError':
+        break;
+      default:
+        break;
+    }
+  });
+
+player.on('timeupdate', throttle(onPlay, 1000));
